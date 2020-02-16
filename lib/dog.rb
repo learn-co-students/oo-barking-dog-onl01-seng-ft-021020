99bb54dc049bf12938052096aba0a83e5043a8d7
Lester Young <lyoung070789@gmail.com>
2020-02-16 04:12:02 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Dog 
+  #Setter
+  def name=(dog_name)
+    @this_is_dogs_name = dog_name
+  end
+  
+  #Getter
+  def name 
+    @this_is_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+  
+end 
